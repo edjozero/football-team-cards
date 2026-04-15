f3834ea072bdd4d10a5cc6d5b82fa438ec1c6a28
edjozero <ej21serafin@gmail.com>
2026-04-14 22:56:53 -0700

    Add players and team information to js file. Create variables for displaying team information

---
+++ script.js
@@ -0,0 +1,43 @@
+const footballTeam = {
+    team: "Manchester United",
+    year: 2010,
+    headCoach: "Sir Alex Ferguson",
+    players: [
+        {name: "Edwin van der Sar", position: "goalkeeper"},
+        {name: "Rio Ferdinand", position: "defender"},
+        {name: "Nemanja Vidić", position: "defender", isCaptain: true},
+        {name: "Patrice Evra", position: "defender"},
+        {name: "John O'Shea", position: "defender"},
+        {name: "Rafael", position: "defender"},
+        {name: "Fabio", position: "defender"},
+        {name: "Jonny Evans", position: "defender"},
+        {name: "Chris Smalling", position: "defender"},
+        {name: "Wes Brown", position: "defender"},
+        {name: "Ritchie De Laet", position: "defender"},
+        {name: "Ryan Giggs", position: "midfielder"},
+        {name: "Paul Scholes", position: "midfielder"},
+        {name: "Nani", position: "midfielder"},
+        {name: "Michael Carrick", position: "midfielder"},
+        {name: "Darren Fletcher", position: "midfielder"},
+        {name: "Antonio Valencia", position: "midfielder"},
+        {name: "Anderson", position: "midfielder"},
+        {name: "Park Ji-sung", position: "midfielder"},
+        {name: "Darron Gibson", position: "midfielder"},
+        {name: "Owen Hargreaves", position: "midfielder"},
+        {name: "Wayne Rooney", position: "forward"},
+        {name: "Dimitar Berbatov", position: "forward"},
+        {name: "Javier Hernández", position: "forward"},
+        {name: "Michael Owen", position: "forward"},
+        {name: "Federico Macheda", position: "forward"},
+        {name: "Gabriel Obertan", position: "forward"}
+    ]
+};
+
+const team = document.getElementById("team");
+const year = document.getElementById("year");
+const headCoach = document.getElementById("head-coach");
+const playersList = document.getElementById("player-cards");
+
+team.textContent = footballTeam.team;
+year.textContent = footballTeam.year;
+headCoach.textContent = footballTeam.headCoach;
